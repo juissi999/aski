a39188dabff119acfd934b1a396008d945b29a52
Jussi Kaatiala <jussi.kaatiala@gmail.com>
2018-05-11 13:23:16 +0300

    Added readme + style fixes.

---
+++ README.md
@@ -0,0 +1,2 @@
+# aski
+A few code-line graphics engine to work with most browsers.
--- aski.js
+++ aski.js
@@ -1,24 +1,23 @@
 // Aski is an ascii javascript game-engine utilising html-tables.
 
 function drawPoint(canvas, x, y, marker){
-    // Draws a single point in the canvas. Checks that the point
-    // is inside canvas.
-    if (y < canvas.length && -1 < y) {
-        if (x < canvas[y].length && -1 < x) {
-            canvas[y][x] = marker;
-        }
-    }
-    return canvas;
+   // Draws a single point in the canvas. Checks that the point
+   // is inside canvas.
+   if (y < canvas.length && -1 < y) {
+      if (x < canvas[y].length && -1 < x) {
+         canvas[y][x] = marker;
+      }
+   }
+   return canvas;
 }
 
 function generateCanvas(size, filler){
-    // Create canvas with wanted size and initialization filler value.
-    var canvas = [];
-    
-    for (var i = 0; i<size;i++) {
-        canvas.push(Array(size).fill(filler));
-    }
-    return canvas;
+   // Create canvas with wanted size and initialization filler value.
+   var canvas = [];
+   for (var i = 0; i<size;i++) {
+      canvas.push(Array(size).fill(filler));
+   }
+   return canvas;
 }
 
 function drawCanvasWithColors(colormatrix){
